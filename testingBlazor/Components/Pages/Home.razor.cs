7d903f1def5b28f6616ffac68ed232280b77ec78
ge-arty <george.arutinyan@gmail.com>
2023-11-21 22:36:17 +0400

    Add project files.

---
+++ testingBlazor/Components/Pages/Home.razor.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace testingBlazor.Components.Pages
+{
+    public partial class Home
+    {
+        [Parameter]
+        public string Title { get; set; }
+
+        [Parameter(CaptureUnmatchedValues = true)]
+        public Dictionary<string, object> AdditionalAttributes { get; set; }
+
+        [CascadingParameter]
+        public string Color { get; set; }
+
+        [Parameter]
+        public RenderFragment VisitShopContent { get; set; }
+
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+
+            // Log the color to the browser console
+            Console.WriteLine($"Color: {Color}");
+ 
+        }
+    }
+}
